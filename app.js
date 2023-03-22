a932abcc763b0f0dd5c1cda30444793b070bb36e
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-03-22 21:22:14 +0530

    some fixes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const container = document.querySelector(".container");
 const items = document.querySelectorAll(".item");
 const screen = document.querySelector(".screen");
 const del = document.querySelectorAll(".del");
-const regexPattern = /[+-/*%^]|\d+/g;
+const regexPattern = /([0-9]*[.])?[0-9]+|[+-/*%^]/g;
+
 // regex either matches a string containing any of the mentioned operators, or number of any length.
 
 let solve = {
@@ -33,18 +34,40 @@ function calculate(exp) {
 
   //this loop just solves the equation.
   let ans = stack[0];
+
   for (let i = 1; i < stack.length; i = i + 2) {
     ans = solve[stack[i]](ans, stack[i + 1]);
   }
-  return ans;
+
+  if (Number.isInteger(ans)) return ans;
+  return ans.toFixed(2);
 }
 
 function backspace(string) {
   return string.slice(0, -1);
 }
 
+function removeDuplicate(string) {
+  let pattern = /[+-/*^]{2,}/;
+  if (pattern.test(string)) {
+    string = string.slice(0, -2) + string.slice(-1);
+  }
+  if (pattern.test(string.slice(-1))) {
+    string = string.slice(0, -1);
+  }
+  return string;
+}
+
+function variableFontSize() {
+  let area = screen.offsetHeight * screen.offsetWidth;
+  let text = screen.value.length;
+  let fontSize = Math.sqrt(area / text)/2;
+  screen.style.fontSize = fontSize + "px";
+  
+}
+
 container.addEventListener("click", (e) => {
-  console.log(e.target);
+
   switch (e.target.innerText) {
     case "=":
       screen.value = calculate(screen.value);
@@ -56,7 +79,13 @@ container.addEventListener("click", (e) => {
 
     default:
       screen.value += e.target.innerText;
-      console.log("no");
       break;
   }
+  variableFontSize();
+
+});
+
+document.addEventListener("click", () => {
+  let ans = removeDuplicate(screen.value);
+  screen.value = ans;
 });
--- todo.md
+++ todo.md
@@ -1,11 +1,17 @@
 Things that are working :
 1.  calculate button
 2.  clear all btn
+3.  fix multiple operator being entered
+4.  back button //use slice function to achieve it
 
 
 Things that require immediate fixed :
-1.  fix multiple operator being entered
+1.  floating point gives error
+
+Things to learn more about :
+1.  Regex
+2.  DOM
+
 
 Things NOT requiring immediate action :
-1.  back button //use slice function to achieve it
 
